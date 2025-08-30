b85cc5be8a1873ce8e90bf265b11d45fab6e6ef5
Alex-Hab <sam_climate@mail.ru>
2025-08-30 17:17:08 +1000

    2025-08-30

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching } from "../Redux/users-reducer";
+import { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching, toggleFollowingProgress } from "../Redux/users-reducer";
 import axios from 'axios';
 import Users from './Users';
 import Preloader from '../common/preloader/Preloader';
@@ -40,6 +40,8 @@ class UsersContainer extends React.Component {
 				users={this.props.users}
 				follow={this.props.follow}
 				unfollow={this.props.unfollow}
+				toggleFollowingProgress={this.props.toggleFollowingProgress}
+				followingInProgress={this.props.followingInProgress}
 			/>
 		</>
 	}
@@ -51,7 +53,8 @@ let mapStateToProps = (state) => {
 		pageSize: state.usersPage.pageSize,
 		totalUsersCount: state.usersPage.totalUsersCount,
 		currentPage: state.usersPage.currentPage,
-		isFetching: state.usersPage.isFetching
+		isFetching: state.usersPage.isFetching,
+		followingInProgress: state.usersPage.followingInProgress,
 	}
 }
 /*let mapDispatchToProps = (dispatch) => {
@@ -82,4 +85,5 @@ let mapStateToProps = (state) => {
 export default connect(mapStateToProps, {
 	follow, unfollow, setUsers,
 	setCurrentPage, setTotalUsersCount, toggleIsFetching,
+	toggleFollowingProgress,
 })(UsersContainer);
